52fd27fb5a76da999c13383366f6008f3123215c
0Nimsay0 <yasmin.dias@alunos.ifsuldeminas.edu.br>
2026-05-18 19:54:03 -0300

    ajuste

--- app/src/main/java/br/edu/ifsuldeminas/mch/constraintlayout/BlogPostActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/mch/constraintlayout/BlogPostActivity.java
@@ -1,12 +1,26 @@
 package br.edu.ifsuldeminas.mch.constraintlayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BlogPostActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_post);
+
+        // Encontra a setinha de voltar do XML
+        ImageView btnVoltar = findViewById(R.id.btnVoltarBlog);
+
+        // Configura o clique para fechar a tela
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Fecha o Blog e volta para a MainActivity
+            }
+        });
     }
 }
